74c1abaebe91e7f844c0c265537f22dcb40f64e4
nguyenphucuit92 <nguyenphucuit@gmail.com>
2015-09-25 11:05:35 +0700

    123

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -11,9 +19,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922134332) do
+ActiveRecord::Schema.define(version: 20121130000001) do
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
